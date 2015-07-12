5c1b48d17d98b831c8aec70bb380d657b8968cd6
botkiller <botkiller@gmail.com>
2015-07-12 13:53:30 -0700

    weeding out last bits of old structure

--- django_project/urls.py
+++ django_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'MTGRewind.coverage.views.home', name='home'),
-    url(r'^(?P<event_id>[0-9]+)/$','MTGRewind.coverage.views.eventdetail', name='eventdetail'),
+    url(r'^$', 'coverage.views.home', name='home'),
+    url(r'^(?P<event_id>[0-9]+)/$','coverage.views.eventdetail', name='eventdetail'),
     url(r'^admin/', include(admin.site.urls)),
 )
