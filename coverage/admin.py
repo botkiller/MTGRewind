090d0c44008630443016249dc52d34591c5a4b21
botkiller <botkiller@gmail.com>
2015-07-12 20:24:33 -0700

    ..

--- coverage/admin.py
+++ coverage/admin.py
@@ -3,6 +3,6 @@ from coverage.models import event
 
 # Register your models here.
 class eventAdmin(admin.ModelAdmin):
-    list_display = ['event_type.name', 'location', 'start_date']
+    list_display = ['location', 'start_date']
 
 admin.site.register(event, eventAdmin)
