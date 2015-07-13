d2af10e8fc47b639657a6285e494946319eda323
botkiller <botkiller@gmail.com>
2015-07-12 23:06:38 -0700

    ..

--- coverage/admin.py
+++ coverage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from coverage.models import event, match, game, deck, format, organizer, player, format_type, archetype, event_type, color, archetype_image, commentator
+from coverage.models import event, match, game, deck, format, organizer, player, format_type, archetype, event_type, color, commentator
 
 # Register your models here.
 class eventAdmin(admin.ModelAdmin):
@@ -8,4 +8,15 @@ class eventAdmin(admin.ModelAdmin):
         return obj.event_type.name
     get_event_type.short_description = "Event Type"
 
-admin.site.register(event, match, game, deck, format, organizer, player, format_type, archetype, event_type, color, archetype_image, commentator, eventAdmin)
+admin.site.register(event,eventAdmin)
+admin.site.register(match)
+admin.site.register(game)
+admin.site.register(deck)
+admin.site.register(format)
+admin.site.register(organizer)
+admin.site.register(player)
+admin.site.register(format_type)
+admin.site.register(archetype)
+admin.site.register(event_type)
+admin.site.register(color)
+admin.site.register(commentator)
