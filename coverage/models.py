a4ea4bb6e517455ae9081277a93d84284c2d4412
botkiller <botkiller@gmail.com>
2015-07-12 20:07:21 -0700

    ..

--- coverage/models.py
+++ coverage/models.py
@@ -10,6 +10,8 @@ class event(models.Model):
     format = models.ForeignKey('coverage.format')
     winning_deck = models.ForeignKey('coverage.deck', blank=True, null=True)
     details_url = models.CharField(max_length=255, blank=True, null=True)
+    def __unicode__(self):
+        return self.location
 
 class match(models.Model):
     event = models.ForeignKey('coverage.event')
